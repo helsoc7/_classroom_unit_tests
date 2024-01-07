4ca4d37757d1d8e7d80de9012e3b9cf9554e4082
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-01-07 14:18:18 +0100

    Update test_is_even.py

--- test_is_even.py
+++ test_is_even.py
@@ -5,13 +5,13 @@ from is_even import is_even
 
 
 class Test(unittest.TestCase):
-    def test_file_exists(self):
-        file_name = self.__module__.split(".")[-1] + ".py"
-        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
-
-    
     def test_is_even(self):
         self.assertTrue(is_even(2))
         self.assertFalse(is_even(3))
         self.assertTrue(is_even(0))
         self.assertFalse(is_even(-1))
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
+
+    
