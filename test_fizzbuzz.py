f581b524fd3384c0fb41f59bc5c773a9f338e491
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-01-07 14:52:26 +0100

    Update test_fizzbuzz.py

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -8,6 +8,6 @@ class Test(unittest.TestCase):
         self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
 
     def test_fizzbuzz(self):
-        self.assertEqual(fizz_buzz(3), "1, 2, Fizz")
-        self.assertEqual(fizz_buzz(5), "1, 2, Fizz, 4, Buzz")
-        self.assertEqual(fizz_buzz(15), "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz")
+        self.assertEqual(fizzbuzz(3), "1, 2, Fizz")
+        self.assertEqual(fizzbuzz(5), "1, 2, Fizz, 4, Buzz")
+        self.assertEqual(fizzbuzz(15), "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz")
