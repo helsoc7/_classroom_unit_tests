ce7c3c982e457381240d5023040733b922f2d1cb
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-01-16 16:35:32 +0100

    Create test_student_manager.py

---
+++ test_student_manager.py
@@ -0,0 +1,20 @@
+import unittest
+from student_manager import Student, StudentManager
+
+class TestStudentManager(unittest.TestCase):
+    def setUp(self):
+        self.manager = StudentManager()
+        self.manager.add_student(Student("Anna", 20))
+        self.manager.add_student(Student("Tom", 22))
+
+    def test_greet(self):
+        student = self.manager.get_student("Anna")
+        self.assertEqual(student.greet(), "Hallo, ich bin Anna!")
+
+    def test_get_student(self):
+        self.assertIsNotNone(self.manager.get_student("Anna"))
+        self.assertIsNone(self.manager.get_student("Bob"))
+
+    def test_remove_student(self):
+        self.manager.remove_student("Tom")
+        self.assertIsNone(self.manager.get_student("Tom"))
