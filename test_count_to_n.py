1c12e86bdfe530113547da4149ef5470d26a6e02
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 11:09:13 +0200

    Create test_count_to_n.py

---
+++ test_count_to_n.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+from io import StringIO
+from count_to_n import count_to_n
+import sys
+
+class TestCountToN(unittest.TestCase):
+    def test_count_to_n(self):
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        count_to_n(5)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue().strip(), "1\n2\n3\n4\n5")
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        count_to_n(3)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue().strip(), "1\n2\n3")
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist
