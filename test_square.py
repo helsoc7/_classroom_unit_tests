19daa68a2ac58e83d1078c84d96d6d32489ce991
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 10:57:03 +0200

    Create test_square.py

---
+++ test_square.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from square import square
+
+class TestSquare(unittest.TestCase):
+    def test_square(self):
+        self.assertEqual(square(3), 9)
+        self.assertEqual(square(-2), 4)
+        self.assertEqual(square(0), 0)
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
