55aaed0b2ee3bdec521c242f6bc53ff1ddd4ed85
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 11:06:11 +0200

    Create test_check_age.py

---
+++ test_check_age.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from check_age import check_age
+
+class TestCheckAge(unittest.TestCase):
+    def test_check_age(self):
+        self.assertEqual(check_age(20), 'volljährig')
+        self.assertEqual(check_age(16), 'minderjährig')
+        self.assertEqual(check_age(18), 'volljährig')
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
