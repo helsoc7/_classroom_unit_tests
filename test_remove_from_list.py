4c4249856ffd39c06b8b7786dc17c9bd823211e9
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 10:59:36 +0200

    Create test_remove_from_list.py

---
+++ test_remove_from_list.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from remove_from_list import remove_from_list
+
+class TestRemoveFromList(unittest.TestCase):
+    def test_remove_from_list(self):
+        self.assertEqual(remove_from_list([1, 2, 3], 2), [1, 3])
+        self.assertEqual(remove_from_list([1, 2, 3], 4), [1, 2, 3])
+        self.assertEqual(remove_from_list([], 4), [])
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
