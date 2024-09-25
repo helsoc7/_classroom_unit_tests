bfe32ca762c1a2e2c003571ddaa4543938bf51c7
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 10:51:59 +0200

    Create test_string_length.py

---
+++ test_string_length.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from string_length import string_length
+
+class TestStringLength(unittest.TestCase):
+    def test_string_length(self):
+        self.assertEqual(string_length("hello"), 5)
+        self.assertEqual(string_length("Python"), 6)
+        self.assertEqual(string_length(""), 0)
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
