76774945207c7912c672d5692d00bee3cbdbe769
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 10:55:47 +0200

    Create test_multiply.py

---
+++ test_multiply.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from multiply import multiply
+
+class TestMultiply(unittest.TestCase):
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 4), 12)
+        self.assertEqual(multiply(0, 5), 0)
+        self.assertEqual(multiply(-2, 6), -12)
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
