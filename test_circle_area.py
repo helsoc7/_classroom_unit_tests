93527c1288bb494220193f97938590af36a00c57
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 10:52:28 +0200

    Create test_circle_area.py

---
+++ test_circle_area.py
@@ -0,0 +1,14 @@
+import unittest
+import os
+from math import isclose
+from circle_area import circle_area
+
+class TestCircleArea(unittest.TestCase):
+    def test_circle_area(self):
+        self.assertTrue(isclose(circle_area(3), 28.27, rel_tol=1e-2))
+        self.assertEqual(circle_area(0), 0)
+        self.assertTrue(isclose(circle_area(2), 12.57, rel_tol=1e-2))
+    
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
