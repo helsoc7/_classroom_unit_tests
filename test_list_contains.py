ae6a301fa5803d86de1a7b9d47525cc4d7245cc6
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 11:10:27 +0200

    Create test_list_contains.py

---
+++ test_list_contains.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from list_contains import list_contains
+
+class TestListContains(unittest.TestCase):
+    def test_list_contains(self):
+        self.assertTrue(list_contains([1, 2, 3], 2))
+        self.assertFalse(list_contains([1, 2, 3], 4))
+        self.assertFalse(list_contains([], 1))
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
