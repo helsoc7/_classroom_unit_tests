940a1738a90ddecdecd176c534c32b77c6ef41d3
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 10:56:14 +0200

    Create test_min_of_three.py

---
+++ test_min_of_three.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from min_of_three import min_of_three
+
+class TestMinOfThree(unittest.TestCase):
+    def test_min_of_three(self):
+        self.assertEqual(min_of_three(3, 4, 5), 3)
+        self.assertEqual(min_of_three(10, 2, 8), 2)
+        self.assertEqual(min_of_three(-1, 0, 1), -1)
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
