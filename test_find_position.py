4b001803544b4d2354b93536775e9fc4a8517c64
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 11:00:10 +0200

    Create test_find_position.py

---
+++ test_find_position.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from find_position import find_position
+
+class TestFindPosition(unittest.TestCase):
+    def test_find_position(self):
+        self.assertEqual(find_position([1, 2, 3], 2), 1)
+        self.assertEqual(find_position([1, 2, 3], 4), -1)
+        self.assertEqual(find_position([], 1), -1)
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
