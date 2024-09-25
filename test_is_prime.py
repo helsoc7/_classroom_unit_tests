4df183cf7218cd5e63c74ed910aa95387b508ed4
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 10:48:47 +0200

    Create test_is_prime.py

---
+++ test_is_prime.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+from is_prime import is_prime
+
+class TestIsPrime(unittest.TestCase):
+    def test_is_prime(self):
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(3))
+        self.assertFalse(is_prime(4))
+        self.assertTrue(is_prime(5))
+        self.assertFalse(is_prime(1))
+        self.assertFalse(is_prime(0))
+        self.assertFalse(is_prime(-3))
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
