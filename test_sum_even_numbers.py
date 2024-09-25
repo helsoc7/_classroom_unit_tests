e49729614960cfc823b04ac1b965ed88ff351855
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-09-25 11:13:21 +0200

    Create test_sum_even_numbers.py

---
+++ test_sum_even_numbers.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from sum_even_numbers import sum_even_numbers
+
+class TestSumEvenNumbers(unittest.TestCase):
+    def test_sum_even_numbers(self):
+        self.assertEqual(sum_even_numbers([1, 2, 3, 4, 5]), 6)
+        self.assertEqual(sum_even_numbers([7, 9, 11]), 0)
+        self.assertEqual(sum_even_numbers([0, 2, 4]), 6)
+
+    def test_file_exists(self):
+        file_name = self.__module__.split(".")[-1] + ".py"
+        self.assertTrue(os.path.isfile(file_name), f"{file_name} does not exist")
