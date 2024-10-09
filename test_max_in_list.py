9ded26d490d0563d305b68aac8884b058ccc2c17
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-10-09 14:31:13 +0200

    Update test_max_in_list.py

--- test_max_in_list.py
+++ test_max_in_list.py
@@ -2,7 +2,7 @@ import unittest
 import os
 from max_in_list import max_in_list
 
-class TestMaxInList(unittest.TestCase):
+class Test(unittest.TestCase):
     def test_max_in_list(self):
         self.assertEqual(max_in_list([1, 2, 3, 4]), 4)
         self.assertEqual(max_in_list([-1, -2, -3, -4]), -1)
